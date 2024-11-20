29d5baa6012597cd2ae54cb57f561b8ee1722cb3
gsduarte06 <40220246@esmad.ipp.pt>
2024-11-20 11:20:41 +0000

    Minor changes

--- src/views/EventPageView.vue
+++ src/views/EventPageView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="d-flex flex-column flex-1-1">
+    <div class="h-5">
+      <v-btn color="success">text</v-btn>
+    </div>
+    <div class="h-5">
+      <v-btn color="success">text</v-btn>
+    </div>
+    <div class="h-5">
+      <v-btn color="success">text</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
