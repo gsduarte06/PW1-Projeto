2538cb6eb514f9978b0c99124235176692701d15
gsduarte06 <40220246@esmad.ipp.pt>
2024-11-20 11:21:06 +0000

    navbar navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-toolbar color="#00041F" height="80">
-      <v-img src="./src/assets/Logo.png" max-height="60" max-width="133" cover> </v-img>
+      <v-img src="./src/assets/Logo.png" max-height="60" max-width="133" cover @click="ToLandingPage"> </v-img>
 
       <v-spacer></v-spacer>
 
@@ -9,13 +9,8 @@
       <v-btn @click="LeaderboardPage" class="mr-md-4 font-weight-bold"> LeaderBoard </v-btn>
       <v-btn class="mr-md-4 font-weight-bold"> Merchandising </v-btn>
 
-      <v-btn
-        @click="LoginRegister"
-        style="background: #ff00ee"
-        class="rounded-pill font-weight-bold px-6"
-        rounded
-        >Login/Register</v-btn
-      >
+      <v-btn @click="LoginRegister" style="background: #ff00ee" class="rounded-pill font-weight-bold px-6"
+        rounded>Login/Register</v-btn>
     </v-toolbar>
   </div>
 </template>
@@ -27,9 +22,12 @@ export default {
   methods: {
     LoginRegister() {
       this.$router.push('/login')
-    },LeaderboardPage() {
+    }, LeaderboardPage() {
       this.$router.push('/leaderboard')
     },
+    ToLandingPage() {
+      this.$router.push('/')
+    }
   },
 }
 </script>
