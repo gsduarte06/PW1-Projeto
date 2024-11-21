5175fd3fbfe4e8425bf6c7e85fc39302ef850bc5
gsduarte06 <40220246@esmad.ipp.pt>
2024-11-21 23:00:59 +0000

    changes on logo, favicon, footer and navbar for bugs

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
     <v-layout>
-      <v-footer
-        height="682"
-        class="text-white d-flex flex-column justify-end"
-        style="background: linear-gradient(#00041f, #59398e); min-width: 100vw"
-      >
-        <v-img src="./src/assets/Logo.png" max-height="180" width="400"> </v-img>
+      <v-footer height="682" class="text-white d-flex flex-column justify-end"
+        style="background: linear-gradient(#00041f, #59398e); min-width: 100vw">
+        <v-img src='./src/assets/Logo.png' max-height="180" width="400"> </v-img>
 
         <div class="pb-10">
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            :icon="icon"
-            class="mx-4"
-            variant="text"
-            color="#00CC99"
-          ></v-btn>
+          <v-btn v-for="icon in icons" :key="icon" :icon="icon" class="mx-4" variant="text" color="#00CC99"></v-btn>
         </div>
 
         <v-divider></v-divider>
@@ -29,6 +19,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      logo: "./src/assets/Logo.png"
+    }
+  },
   data: () => ({
     icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
   }),
